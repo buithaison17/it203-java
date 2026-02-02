8ca8763737d19deb42090c4362e8609fc66887aa
sonbui <thaisonb936@gmail.com>
2026-02-02 19:19:55 +0700

    upload sesion07

---
+++ session07/Bai6.java
@@ -0,0 +1,16 @@
+class User {
+    final String userId;
+
+    public User(String userId) {
+        this.userId = userId;
+    }
+}
+
+class UserManager {
+}
+
+public class Bai6 {
+    public static void main(String[] args) {
+        User u = new User("1");
+    }
+}
