afbe35e283462eb7834fca39aa25c4099bb3341a
sonbui <thaisonb936@gmail.com>
2026-02-06 08:24:57 +0700

    update bai 5,6

--- session09/Bai5.java
+++ session09/Bai5.java
@@ -1,51 +1,49 @@
-class Employee {
-    double hours;
+abstract class Employee {
+    String name;
 
-    public Employee(double hours) {
-        this.hours = hours;
+    public Employee(String name) {
+        this.name = name;
     }
 
-    public double calculateSalary() {
-        return 0;
-    }
-
+    abstract double calculateSalary();
 }
 
 class OfficeEmployee extends Employee {
-    public OfficeEmployee(double hours) {
-        super(hours);
+    double baseSalary;
+
+    public OfficeEmployee(String name, double baseSalary) {
+        super(name);
+        this.baseSalary = baseSalary;
     }
 
     @Override
-    public double calculateSalary() {
-        return 50000 * hours;
+    double calculateSalary() {
+        return baseSalary;
     }
 }
 
 class ProductionEmployee extends Employee {
-    public ProductionEmployee(double hours) {
-        super(hours);
+    int numOfProducts;
+    double price;
+
+    public ProductionEmployee(String name, int numOfProducts, double price) {
+        super(name);
+        this.numOfProducts = numOfProducts;
+        this.price = price;
     }
 
     @Override
-    public double calculateSalary() {
-        return 100000 * hours;
+    double calculateSalary() {
+        return numOfProducts * price;
     }
 }
 
 public class Bai5 {
     public static void main(String[] args) {
-        Employee[] employees = new Employee[3];
-        employees[0] = new OfficeEmployee(3);
-        employees[1] = new ProductionEmployee(5);
-        employees[2] = new OfficeEmployee(4);
-
-        double totalSalary = 0;
-
-        for (Employee employee : employees) {
-            totalSalary += employee.calculateSalary();
-        }
-
-        System.out.println(totalSalary);
+        OfficeEmployee oe = new OfficeEmployee("son", 1000);
+        System.out.println(oe.calculateSalary());
+        ProductionEmployee pe = new ProductionEmployee("son", 10, 5000);
+        System.out.println(pe.calculateSalary());
     }
+
 }
--- session09/Bai6.java
+++ session09/Bai6.java
@@ -1,5 +1,49 @@
+class Shape {
+    public double calculateArea() {
+        return 0;
+    };
+}
+
+class Circle extends Shape {
+    double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle extends Shape {
+    double width;
+    double height;
+
+    public Rectangle(double width) {
+        this.width = width;
+        this.height = width;
+    }
+
+    public Rectangle(double width, double height) {
+        this.height = height;
+        this.width = width;
+    }
+
+    @Override
+    public double calculateArea() {
+        return height * width;
+    }
+}
+
 public class Bai6 {
     public static void main(String[] args) {
-
+        Rectangle r1 = new Rectangle(2);
+        Rectangle r2 = new Rectangle(2, 3);
+        Circle c = new Circle(5);
+        System.out.println(r1.calculateArea());
+        System.out.println(r2.calculateArea());
+        System.out.println(c.calculateArea());
     }
 }
